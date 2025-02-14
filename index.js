6d5ab4307ae8fe8ab9ad79d1c92951bdc57d4dae
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-02-14 12:03:54 -0300

    condicionais

---
+++ index.js
@@ -0,0 +1,66 @@
+// let porta = false;
+
+// if(porta){
+//     console.log("Sair");
+// }
+
+// let idade = 18;
+
+// if(idade > 18){
+//     console.log("Pode dirigir");
+// }else{
+//     console.log("Não pode dirigir");
+// }
+
+// let semaforo = "";
+
+// if(semaforo == "verde"){
+//     console.log("Siga em frente");
+// } else if(semaforo == "amarelo"){
+//     console.log("Aceleraaaaaa!!!");
+// } else if(semaforo == ""){
+//     console.log("Insira um valor");
+// } else {
+//     console.log("Pare");
+// }
+
+// if(semaforo == "verde"){
+//     console.log("Siga em frente");
+// }
+// if(semaforo == "amarelo"){
+//     console.log("Aceleraaaaaa!!!");
+// }
+// if(semaforo == "vermelho"){
+//     console.log("Pare");
+// }
+// if(semaforo == ""){
+//     console.log("Insira um valor");
+// }
+
+// if((2 === "2") || (2 > 1)){
+//     console.log("Sim");
+// }
+// if((2 === "2") && (2 > 1)){
+//     console.log("Sim");
+// }
+
+let mes = "Fevereiro";
+let dia = 2;
+let nomeDeForrozeiro = "";
+
+if(mes == "Janeiro"){
+    nomeDeForrozeiro = "Forrozeiro";
+} else if(mes == "Fevereiro"){
+    nomeDeForrozeiro = "Avexado(a)";
+} else if(mes == "Março"){
+    nomeDeForrozeiro = "Arretado(a)";
+}
+
+if(dia == 1){
+    nomeDeForrozeiro += " Do Milho";
+}else if(dia == 2){
+    nomeDeForrozeiro += " Da Quadrilha";
+}
+
+
+console.log(nomeDeForrozeiro);
